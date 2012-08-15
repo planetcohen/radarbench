3a52cce8015d552d98756d6f6b75500129f9242e
Michael Cohen <michael@planetcohen.com>
2012-08-15 14:12:39 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Radarbench::Application.config.secret_token = '948da8cb0578fd1465ef93923cb61c449b423cef33e7ed49a6353aba5c07327830b88d73843791fdd7876273d681d868d5382dbcd00a2ff1fd0a6148e69a70a1'
